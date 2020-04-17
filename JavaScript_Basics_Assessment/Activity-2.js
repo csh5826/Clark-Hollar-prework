b334535b810b690ae13bf883700262667a12852d
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-04-17 16:29:39 -0400

    javascript basics done

---
+++ JavaScript_Basics_Assessment/Activity-2.js
@@ -0,0 +1,6 @@
+const name = prompt('What is your name?');
+if (name.length > 4) {
+    alert('Your name is greater than four characters.');
+  } else {
+    alert('Your name is 4 characters or less.');
+  }
---
+++ JavaScript_Basics_Assessment/Activity-3.js
@@ -0,0 +1,12 @@
+studentNames = ['Ron', 'Fred', 'George'];
+for (let i = 0; i < 3; i++){
+    const name = prompt('enter a name');
+    studentNames.push(name);
+}
+
+for (let i = 0; i < studentNames.length; i++){
+    const student = studentNames[i];
+    console.log(student); //alert(student);<-use this instead of console.log to run it correctly via the html activity 3 provided
+}
+
+// for whatever reason the terminal i use (mac) to run my javascript will not allow prompts()
