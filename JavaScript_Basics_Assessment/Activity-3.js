aa4ace7d9c1375ce4a106b7610a71f468547b4bb
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-04-17 19:43:10 -0400

    added some comments to my activity-3 JS file

--- JavaScript_Basics_Assessment/Activity-3.js
+++ JavaScript_Basics_Assessment/Activity-3.js
@@ -6,7 +6,9 @@ for (let i = 0; i < 3; i++){
 
 for (let i = 0; i < studentNames.length; i++){
     const student = studentNames[i];
-    console.log(student); //alert(student);<-use this instead of console.log to run it correctly via the html activity 3 provided
+    console.log(student); //alert(student);<-use this instead of console.log to run it and have the names popup at the end via the html activity 3 provided
 }
+// if you use the console.log the names did not pop up for me in firefox or chrome
+// if you command->option-> j in chrome (k for firefox) the console will open up and show the names listed
 
 // for whatever reason the terminal i use (mac) to run my javascript will not allow prompts()
